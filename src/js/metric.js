353fc7a51bcc234d0f5667ce55e7fe54c99d79bf
Chris Mackey <chris@mackeyarchitecture.com>
2018-07-21 06:48:23 -0400

    Update Metric

--- src/js/metric.js
+++ src/js/metric.js
@@ -1,9 +1,8 @@
 
-function precisionRound(number, precision) {
+precisionRound = function(number, precision) {
   		var factor = Math.pow(10, precision);
   		return Math.round(number * factor) / factor;
 		}
-
 
 buildMetric = function(metricValue, propertiesDict) {
   // set default values
@@ -11,7 +10,7 @@ buildMetric = function(metricValue, propertiesDict) {
   decimals = 1
   units = ""
   longName = ""
-  id = '_' + Math.random().toString(36).substr(2, 9)
+  id = genID()
 
   // check the properties dictionary to see if any are overridden
   if ("width" in propertiesDict){
@@ -35,10 +34,21 @@ buildMetric = function(metricValue, propertiesDict) {
 
   $('#metrics').append(
    '<div class= "metricdiv" style= "'+ styleString +'">'+
-   '<div class= "metricval" id=" '+ id +' ">' + precisionRound(metricValue, decimals).toString() +
+   '<div class= "metricval" id="'+ id +'">' + precisionRound(metricValue, decimals).toString() +
    '</div>' +
    '<p>' + units + '<br>' + longName + '</p>'+
    '</div>');
 
   return id
+}
+
+updateMetric = function(id, num) {
+  uId = "#" + id.toString()
+  d3.select(uId).remove()
+  d3.select(uId).append("text")
+        .attr("text-anchor", "start")
+        .attr("fill", "#000")
+    		.style('font-size', '30px')
+    		.style('font-family', 'sans-serif')
+   			.text(num.toString());
 }

a509f14d7d1eb30254f350407475148c30c5839f
Mingbo Peng <mingo1214@gmail.com>
2018-08-06 21:06:59 -0400

    init TS dev

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+
+function greeter(person:string) {
+    return "Hello, " + person;
+}
+
+let user = "MP";
+
+document.body.innerHTML = greeter(user);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only"
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  externals: {
+    'd3': 'd3',
+    'jquery': 'jquery'
+}
+};
